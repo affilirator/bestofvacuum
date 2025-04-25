15443a2405d2f3cbdf6b7521ecda31df527c8dfb
Patrick Mahinge <hello@mahinge.com>
2025-04-25 20:49:21 +0300

    Added Prose Class

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,19 @@ export default {
           '900': '#330000',
           '950': '#190000',
         },
+        accent: {
+          '50': '#f0f9ff',
+          '100': '#e0f2fe',
+          '200': '#bae6fd',
+          '300': '#7dd3fc',
+          '400': '#38bdf8',
+          '500': '#0ea5e9',
+          '600': '#0284c7',
+          '700': '#0369a1',
+          '800': '#075985',
+          '900': '#0c4a6e',
+          '950': '#082f49',
+        },
         secondary: {
           '50': '#fffaeb',
           '100': '#fef3c7',
@@ -37,5 +50,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography'),],
 };
